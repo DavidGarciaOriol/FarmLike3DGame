eb43d8ff25ae6b0c060be7c9a58b242ccc6edb38
DavidGarciaOriol <davidgarciaoriol@gmail.com>
2025-05-17 15:34:07 +0200

    Agregada lógica de cambios de estado y materiales del terreno

---
+++ Assets/Scripts/Terreno.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Terreno : MonoBehaviour
+{
+    [Header("Materiales del terreno")]
+    [SerializeField]
+    private Material terrenoHierba, terrenoArado, terrenoRegado;
+
+    // Rendereizador del terreno
+    private Renderer renderizadorTerreno;
+
+    // Estados del terreno
+    private enum EstadoTerreno
+    {
+        Hierba,
+        Arado,
+        Regado
+    }
+
+    // Estado actual del terreno
+    private EstadoTerreno estadoActualTerreno;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Componente de renderizado del terreno
+        renderizadorTerreno = GetComponent<Renderer>();
+
+        // Estado por defecto del terreno
+        CambiarEstadoTerreno(EstadoTerreno.Hierba);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Cambios de estado del terreno
+    void CambiarEstadoTerreno(EstadoTerreno nuevoEstado)
+    {
+        EstadoTerreno estadoActual = estadoActualTerreno;
+        estadoActualTerreno = nuevoEstado;
+
+        // Llama al cambio de material acorde al nuevo estado
+        CambiarMaterialTerreno(nuevoEstado);
+    }
+
+    // Cambia el material del terreno según el estado del mismo
+    void CambiarMaterialTerreno(EstadoTerreno estadoTerreno)
+    {
+        Material nuevoMaterial = terrenoHierba;
+
+        switch (estadoTerreno)
+        {
+            case EstadoTerreno.Hierba:
+                // Cambia a material hierba
+                nuevoMaterial = terrenoHierba;
+                break;
+
+            case EstadoTerreno.Arado:
+                // Cambia a material terreno arado
+                nuevoMaterial = terrenoArado;
+                break;
+
+            case EstadoTerreno.Regado:
+                // Cambia a material terreno regado
+                nuevoMaterial = terrenoRegado;
+                break;
+        }
+
+        // Llama al renderizador para que muestre el nuevo material
+        CambiarRenderizadoTerreno(nuevoMaterial);
+    }
+
+    // Se encarga de modificar el renderizador para mostrar el nuevo material
+    void CambiarRenderizadoTerreno(Material nuevoMaterialRenderizado)
+    {
+        renderizadorTerreno.material = nuevoMaterialRenderizado;
+    }
+}
